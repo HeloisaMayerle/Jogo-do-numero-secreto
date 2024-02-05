644fe5884c9f7e4822fda04b726b42dd50346798
Heloisa Mayerle Lamego <110624387+HeloisaMayerle@users.noreply.github.com>
2024-02-05 12:49:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -67,5 +67,5 @@ function reiniciarJogo() {
   limparCampo()
   tentativas = 1
   exibirMensagemInicial()
-  document.getElementById("reiniciar").setAttribute("disabled", true)
+  document.getElementById("reiniciar").setAttribute("enable", true)
 }
